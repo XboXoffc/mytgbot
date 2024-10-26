772dd04be6890754fa9c5c6af87e52fc723ce525
XboXoffc <149091168+XboXoffc@users.noreply.github.com>
2024-10-26 22:57:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import asyncio
 import requests
 
 TOKEN = "YOUR TOKEN"
-API_KEY = "YOUR TOKEN" #IN https://www.weatherapi.com/
+API_KEY = "YOUR TOKEN" #https://www.weatherapi.com/
 bot = AsyncTeleBot(TOKEN, "MARKDOWN")
 
-@bot.message_handler(commands=["start"], chat_types="private")
+
+
+@bot.message_handler(commands=["start", "help"], chat_types="private")
 async def start(message):
+    gtm(message)
     await bot.send_message(message.chat.id, 
     """*Hello*
     My commands:
@@ -16,10 +19,12 @@ async def start(message):
 
 @bot.message_handler(commands=["dice"], chat_types="private")
 async def dice(message):
+    gtm(message)
     await bot.send_dice(message.chat.id)
 
 @bot.message_handler(commands=["weather"])
 async def weather(message):
+    gtm(message)
     message_split = message.text.split(" ")
     city = message_split[1]
     days = 2
@@ -53,4 +58,28 @@ async def weather(message):
     else:
         await bot.send_message(message.chat.id, f"api is not work {response}")
 
+#Подслушка
+def gtm(message):
+    if message.text:
+        print('Пользователь {}, {} написал: {}'.format(message.from_user.id, message.from_user.first_name, message.text))
+    elif message.photo:
+        print('Пользователь {}, {} отправил фото: {}'.format(message.from_user.id, message.from_user.first_name, message.photo[0].file_id))
+    elif message.sticker:
+        print('Пользователь {}, {} отправил стикер: {}'.format(message.from_user.id, message.from_user.first_name, message.sticker.emoji))
+    elif message.location:
+        print('Пользователь {}, {} отправил локацию'.format(message.from_user.id, message.from_user.first_name))
+        
+@bot.message_handler(content_types=['text', 'photo', 'document', 'audio', 'video', 'sticker',
+                    'location', 'contact', 'voice', 'poll', 'poll_answer', 'dice', 'game'],
+                    chat_types=['private'])
+async def echo_message(message):
+    gtm(message)
+
+@bot.message_handler(func=lambda message: True, content_types=['text', 'location'])
+async def echo_message2(message):
+    gtm(message)
+
+
+
+print("Бот запущен")
 asyncio.run(bot.polling(non_stop=True))
